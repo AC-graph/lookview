bc81ca3aa405923e477a1d475464001e858696e0
DJ-code-csii <741456520@qq.com>
2020-09-17 14:26:29 +0800

    给path提供点阵数组属性来连线

--- dist/lookview.js
+++ dist/lookview.js
@@ -2718,12 +2718,16 @@
           type: "string",
           "default": "open"
         },
-        x0: $numRequired,
-        y0: $numRequired,
-        x: $numRequired,
-        y: $numRequired,
-        x1: $numOne,
-        y1: $numOne
+        array: {
+          type: "json",
+          required: true
+        } // x0: $numRequired,
+        // y0: $numRequired,
+        // x: $numRequired,
+        // y: $numRequired,
+        // x1: $numOne,
+        // y1: $numOne,
+
       },
       link: function link(painter, attr) {
         painter.config({
@@ -2735,16 +2739,36 @@
         var type = attr.type;
 
         if (type == 'open') {
-          painter.beginPath().moveTo(attr.x0, attr.y0).lineTo(attr.x, attr.y).stroke();
+          painter.beginPath().moveTo(attr.array[0][0], attr.array[0][1]);
+
+          for (var i = 1; i < attr.array.length; i++) {
+            painter.lineTo(attr.array[i][0], attr.array[i][1]);
+          }
+
+          painter.stroke();
         } else if (type == 'close') {
-          painter.beginPath().moveTo(attr.x0, attr.y0).lineTo(attr.x, attr.y).lineTo(attr.x1, attr.y1).closePath().stroke().fill();
+          painter.beginPath().moveTo(attr.array[0][0], attr.array[0][1]);
+
+          for (var _i = 1; _i < attr.array.length; _i++) {
+            painter.lineTo(attr.array[_i][0], attr.array[_i][1]);
+          }
+
+          painter.closePath().stroke().fill();
         } else {
           // 错误提示
           console.error('[LookView error]: Type error!' + JSON.stringify({
             series: "path",
             type: type
           }));
-        }
+        } // if (type == 'open') {
+        //     painter.beginPath().moveTo(attr.x0, attr.y0).lineTo(attr.x, attr.y).stroke();
+        // } else if (type == 'close') {
+        //     painter.beginPath().moveTo(attr.x0, attr.y0).lineTo(attr.x, attr.y).lineTo(attr.x1, attr.y1).closePath().stroke().fill();
+        // } else {
+        //     // 错误提示
+        //     console.error('[LookView error]: Type error!' + JSON.stringify({ series: "path", type }));
+        // }
+
       }
     };
   }];
